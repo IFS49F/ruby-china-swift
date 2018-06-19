fedf51f66c5feb2d9c175351d04c296db8121a37
just4fun <houritsunohikari@gmail.com>
2018-06-19 22:27:24 +0800

    chore: initiate the app

---
+++ ruby-china-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ruby-china-swift
+//
+//  Created by just4fun on 19/06/2018.
+//  Copyright © 2018 ifs49f. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
